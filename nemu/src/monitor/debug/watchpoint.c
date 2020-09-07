464821bbc0573cb7916651193591d5191d79df4e
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-07 11:31:43 +0000

    >  compile NEMU 3018209097 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:31:43 up 2 days,  3:02,  4 users,  load average: 1.16, 1.19, 0.79 ed31c608d030ff5e742bda3765edee5b6d58f09d

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -20,4 +20,17 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
-
+WP* new_wp()
+{
+	WP *f,*p;
+	f = free_;
+	free_ = free_->next;
+	f->next = NULL;
+	p = head;
+	if (p == NULL){head = f;p = head;}
+	else {
+		while (p->next!=NULL)p=p->next;
+		p->next = f;
+		}
+	return f;
+}
