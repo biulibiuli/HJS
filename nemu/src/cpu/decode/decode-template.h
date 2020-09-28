815138da48586979f265d2ad18953f41118f1f36
tracer-ics2016 <tracer@njuics.org>
2020-09-28 17:41:35 +0000

    >  compile NEMU 3018209097 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:41:35 up  1:37,  1 user,  load average: 1.70, 0.85, 0.82 1299c707373226ea6091496bf00241bc71ebe08

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -5,6 +5,7 @@
 #define decode_r_internal concat3(decode_r_, SUFFIX, _internal)
 #define decode_rm_internal concat3(decode_rm_, SUFFIX, _internal)
 #define decode_i concat(decode_i_, SUFFIX)
+#define decode_no_rm_internal concat3(decode_no_rm_, SUFFIX, _internal)
 #define decode_n concat(decode_n_, SUFFIX)
 #define decode_a concat(decode_a_, SUFFIX)
 #define decode_r2rm concat(decode_r2rm_, SUFFIX)
