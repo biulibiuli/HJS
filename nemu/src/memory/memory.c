c92e2b1506ea9a844635c5c3488160bcc39278d1
tracer-ics2016 <tracer@njuics.org>
2020-10-14 15:08:30 +0000

    >  compile NEMU 3018209097 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:08:30 up  2:00,  1 user,  load average: 1.10, 0.70, 0.71 4fe44a666cf2da6884b763a36e45d6ba5299017c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,6 +12,13 @@ uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
 /* Memory accessing interfaces */
+extern void ddr3_read(hwaddr_t, void*);
+extern void ddr3_write(hwaddr_t, void*,uint8_t*);
+lnaddr_t seg_translate(swaddr_t, size_t, uint8_t);
+hwaddr_t page_translate(lnaddr_t);
+int is_mmio(hwaddr_t);
+uint32_t mmio_read(hwaddr_t, size_t, int);
+void mmio_write(hwaddr_t, size_t, uint32_t, int);
 CPU_state cpu;
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
